da666da75562a6fc339cc5b6043f0f397f77db35
ivanhoe <ayvengo85@gmail.com>
2024-01-29 14:53:32 +0200

    29.01.2024

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,82 @@
-from django.db import models
+from django.db.models import EmailField, CharField, BooleanField, DateTimeField
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, name=None, full_name=None,
+                    is_active=True, is_staff=None, is_admin=None):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not email:
+            raise ValueError('Користувач повинен мати електронну адресу')
+        if not password:
+            raise ValueError('Користувач повинен ввести пароль')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.staff = is_staff
+        user.admin = is_admin
+        user.is_active = is_active
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, name=None):
+        user = self.create_user(email, name=name, password=password,
+                                is_staff=True, is_admin=True)
+        return user
+
+    def create_staffuser(self, email, password=None, name=None):
+        user = self.create_user(email, name=name, password=password,
+                                is_staff=True, is_admin=False)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = EmailField(unique=True, max_length=255)
+    name = CharField(max_length=255, blank=True, null=True)
+    full_name = CharField(max_length=255, blank=True, null=True)
+    staff = BooleanField(default=False)
+    is_active = BooleanField(default=True)
+    admin = BooleanField(default=False)
+    timestamp = DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'  # запрашиваемое поле для автаризации
+    REQUIRED_FIELDS = []  # ніякі додаткові поля для авторизаціі не будуть запрашиватся
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def get_short_name(self):  # функція виводить eml замість імя якщо його немає
+        if self.name:
+            return self.name
+        return self.email
+
+    def get_full_name(self):  # функція виводить eml замість повного імя якщо його немає
+        if self.full_name:
+            return self.full_name
+        return self.email
+
+    def has_perm(self, perm, obj=None):  # відповідає за права на обєкти в адмінке
+        return True
+
+    def has_module_perms(self, app_label):  # права на модуль
+        return True
+
+    @property
+    def is_staff(self):
+        if self.admin:
+            return True
+        return self.staff
+
+    @property
+    def is_amin(self):
+        return self.admin
+
+    def save(self, *args, **kwargs): # записує паролі в базу
+        print(self.password)
+        super().save(*args, **kwargs)
--- post_in/settings.py
+++ post_in/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'post_in',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -68,7 +70,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'post_in.wsgi.application'
-
+#AUTH_USER_MODEL = 'accounts.User'
+AUTH_USER_MODEL = 'accounts.User'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
